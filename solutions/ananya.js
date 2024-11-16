2e7976cd765b51a11ede7ed0065204f7ca222aef
Marvin Irwin <marvin@marvinirwin.com>
2024-11-16 22:58:05 +0000

    basic solution

---
+++ solutions/ananya.js
@@ -0,0 +1,13 @@
+const solution1 = {
+    title: "Become more social by integrating your shadow",
+    body: `<p>Instructions</p>Some Youtube embed</p>`,
+    validatorQuestion: "Does this input represent a person integrating their shadow"
+};
+const solution2 = {
+    title: "Use Jungian conditioning",
+    body: "<div>First Slide</div><div>Second Slide</div>",
+    validatorQuestion: "Does this input represent a person understanding themselves through a Jungian lense"
+}
+export const solutions = [
+    solution1
+]
